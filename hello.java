37984375a8c73d83a1bcbdc49d4f75b58fa4154c
JAGAN <jaganmaharana.co@gmail.com>
2026-05-17 20:50:44 +0530

    Week 1

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+     
+    byte a =10;
+    byte b =30;
+    int result = a*b;
+        System.out.println(result);
+    }
+}
---
+++ opearator.java
@@ -0,0 +1,45 @@
+public class opearator {
+    public static void main(String[] args) {
+        // int num1=7;
+        // arithmatic operator
+
+
+       // int num2 =5;
+    //     int result = num1 + num2;
+    //     System.out.println(result);
+    //      result = num1 - num2;
+    //         System.out.println(result);
+    //          result = num1 * num2;
+    //            System.out.println(result);   
+    //          result = num1 / num2; 
+    //            System.out.println(result);  
+    //          result = num1 % num2;
+    //            System.out.println(result);
+    // num1 = num1 + 2;
+    // System.out.println(num1);
+    // num1 +=2;
+    // num1 -=2;
+    // // num1 ++; post
+    // num1 --; 
+    // ++num1;
+    // --num1;
+//    int result = num1 ++;
+// int result = ++ num1;
+
+
+// relational operators
+
+// double x =6.7;
+// double y = 7.7;
+
+// boolean result = x>y;
+// boolean result = x<y;
+// boolean result = x==y;
+// boolean result = x>=y;
+
+// logical ooperators
+
+    System.out.println();
+
+     }
+}
