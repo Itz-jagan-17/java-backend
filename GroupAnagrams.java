cee4c69176b6c134c13c8e6b329d3a3fad2cb997
JAGAN <jaganmaharana.co@gmail.com>
2026-05-23 14:11:51 +0530

    Day 3: Group Anagrams - Character counting approach

---
+++ GroupAnagrams.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<String,List<String>>map =new HashMap<>();
+       for (String word :strs) {
+        int [] count =new int [26];
+            for (char c :word.toCharArray()){
+                count[c-'a']++;
+            
+            }
+            StringBuilder key =new StringBuilder();
+            for(int c : count){
+                key.append(c).append("#");
+            }
+            String keyStr=key.toString();
+            if(!map.containsKey(keyStr)){
+                map.put(keyStr,new ArrayList<>());
+            }
+            map.get(keyStr).add(word);
+       }
+       return new ArrayList<>(map.values());
+    }
+}
